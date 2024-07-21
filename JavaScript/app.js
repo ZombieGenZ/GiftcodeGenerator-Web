9fb67050d68206467bbbae71f6d09e794aa5b939
ZombieGenZ <zombiegenzzz@gmail.com>
2024-07-21 18:41:39 +0700

    Update

---
+++ JavaScript/app.js
@@ -0,0 +1,351 @@
+let value = {
+    "CK1PUA3S51JH": {
+        "giftcode": "CK1PUA3S51JH",
+        "min": 1,
+        "max": 5
+    },
+    "UY4DBK4G1CYF": {
+        "giftcode": "UY4DBK4G1CYF",
+        "min": 1,
+        "max": 5
+    },
+    "YYAO6RC2UNT0": {
+        "giftcode": "YYAO6RC2UNT0",
+        "min": 1,
+        "max": 5
+    },
+    "C6FEZTR2LJ4B": {
+        "giftcode": "C6FEZTR2LJ4B",
+        "min": 1,
+        "max": 5
+    },
+    "YF86URMQ9VE6": {
+        "giftcode": "YF86URMQ9VE6",
+        "min": 1,
+        "max": 5
+    },
+    "5PUK2NEOCMFP": {
+        "giftcode": "5PUK2NEOCMFP",
+        "min": 1,
+        "max": 5
+    },
+    "QORS6LSYO24B": {
+        "giftcode": "QORS6LSYO24B",
+        "min": 1,
+        "max": 5
+    },
+    "TAPKTUPL4U4L": {
+        "giftcode": "TAPKTUPL4U4L",
+        "min": 1,
+        "max": 5
+    },
+    "FGGEJZEDU4G0": {
+        "giftcode": "FGGEJZEDU4G0",
+        "min": 1,
+        "max": 5
+    },
+    "CTWEHRE00OQM": {
+        "giftcode": "CTWEHRE00OQM",
+        "min": 1,
+        "max": 5
+    },
+    "7R87Q78S8W2O": {
+        "giftcode": "7R87Q78S8W2O",
+        "min": 1,
+        "max": 5
+    },
+    "BCF6LPCPFP3K": {
+        "giftcode": "BCF6LPCPFP3K",
+        "min": 1,
+        "max": 5
+    },
+    "6C9KG6HUSQRD": {
+        "giftcode": "6C9KG6HUSQRD",
+        "min": 1,
+        "max": 5
+    },
+    "48NQM0L8MKKP": {
+        "giftcode": "48NQM0L8MKKP",
+        "min": 1,
+        "max": 5
+    },
+    "T6G0YK0QN39W": {
+        "giftcode": "T6G0YK0QN39W",
+        "min": 1,
+        "max": 5
+    },
+    "JVGNCMJL8FON": {
+        "giftcode": "JVGNCMJL8FON",
+        "min": 1,
+        "max": 5
+    },
+    "3GNF0489SBTQ": {
+        "giftcode": "3GNF0489SBTQ",
+        "min": 1,
+        "max": 5
+    },
+    "9ADEVLQ3PA45": {
+        "giftcode": "9ADEVLQ3PA45",
+        "min": 1,
+        "max": 5
+    },
+    "PDUDVHGAZK8P": {
+        "giftcode": "PDUDVHGAZK8P",
+        "min": 1,
+        "max": 5
+    },
+    "ABCTPTPPOA67": {
+        "giftcode": "ABCTPTPPOA67",
+        "min": 1,
+        "max": 5
+    },
+    "2GUQ5OFFDU0E": {
+        "giftcode": "2GUQ5OFFDU0E",
+        "min": 1,
+        "max": 5
+    },
+    "C7VLFH4FSMPH": {
+        "giftcode": "C7VLFH4FSMPH",
+        "min": 1,
+        "max": 5
+    },
+    "MMSKY9TPQZ4V": {
+        "giftcode": "MMSKY9TPQZ4V",
+        "min": 1,
+        "max": 5
+    },
+    "BKZBLGTQK82C": {
+        "giftcode": "BKZBLGTQK82C",
+        "min": 1,
+        "max": 5
+    },
+    "DT4WAF1ZNC1F": {
+        "giftcode": "DT4WAF1ZNC1F",
+        "min": 1,
+        "max": 5
+    },
+    "O9ZD02FYUTY0": {
+        "giftcode": "O9ZD02FYUTY0",
+        "min": 1,
+        "max": 5
+    },
+    "2ZYUCJMUW8UT": {
+        "giftcode": "2ZYUCJMUW8UT",
+        "min": 1,
+        "max": 5
+    },
+    "67A8RKZSLJ2N": {
+        "giftcode": "67A8RKZSLJ2N",
+        "min": 1,
+        "max": 5
+    },
+    "VZ4L2ML2JFFP": {
+        "giftcode": "VZ4L2ML2JFFP",
+        "min": 1,
+        "max": 5
+    },
+    "93GNKJGON7SN": {
+        "giftcode": "93GNKJGON7SN",
+        "min": 1,
+        "max": 5
+    },
+    "434KJQKVG4WL": {
+        "giftcode": "434KJQKVG4WL",
+        "min": 5,
+        "max": 10
+    },
+    "KMMW8772MRFQ": {
+        "giftcode": "KMMW8772MRFQ",
+        "min": 5,
+        "max": 10
+    },
+    "VJQZE5VO8W8K": {
+        "giftcode": "VJQZE5VO8W8K",
+        "min": 5,
+        "max": 10
+    },
+    "68R9NL9Z00K2": {
+        "giftcode": "68R9NL9Z00K2",
+        "min": 5,
+        "max": 10
+    },
+    "U0T4YW1G49Y4": {
+        "giftcode": "U0T4YW1G49Y4",
+        "min": 5,
+        "max": 10
+    },
+    "KALLEHH07PBR": {
+        "giftcode": "KALLEHH07PBR",
+        "min": 5,
+        "max": 10
+    },
+    "T5VLYGGSWJO0": {
+        "giftcode": "T5VLYGGSWJO0",
+        "min": 5,
+        "max": 10
+    },
+    "4LUEQWG0O45A": {
+        "giftcode": "4LUEQWG0O45A",
+        "min": 5,
+        "max": 10
+    },
+    "79CUW740O0SR": {
+        "giftcode": "79CUW740O0SR",
+        "min": 5,
+        "max": 10
+    },
+    "5ZMMPP4DNF1Y": {
+        "giftcode": "5ZMMPP4DNF1Y",
+        "min": 5,
+        "max": 10
+    },
+    "VE8QAK9YW14D": {
+        "giftcode": "VE8QAK9YW14D",
+        "min": 5,
+        "max": 10
+    },
+    "6REF9W17W0ZM": {
+        "giftcode": "6REF9W17W0ZM",
+        "min": 5,
+        "max": 10
+    },
+    "AVZTUZTVS1PJ": {
+        "giftcode": "AVZTUZTVS1PJ",
+        "min": 5,
+        "max": 10
+    },
+    "L5EGKLOVWFH4": {
+        "giftcode": "L5EGKLOVWFH4",
+        "min": 5,
+        "max": 10
+    },
+    "VFOFEBZGBZLT": {
+        "giftcode": "VFOFEBZGBZLT",
+        "min": 5,
+        "max": 10
+    },
+    "AT3JFF92W59O": {
+        "giftcode": "AT3JFF92W59O",
+        "min": 5,
+        "max": 10
+    },
+    "J86SNJO27WPO": {
+        "giftcode": "J86SNJO27WPO",
+        "min": 5,
+        "max": 10
+    },
+    "2Z7NMYJ8HTYZ": {
+        "giftcode": "2Z7NMYJ8HTYZ",
+        "min": 5,
+        "max": 10
+    },
+    "BPEMS51G7A71": {
+        "giftcode": "BPEMS51G7A71",
+        "min": 5,
+        "max": 10
+    },
+    "VKS7KNW1ME4F": {
+        "giftcode": "VKS7KNW1ME4F",
+        "min": 5,
+        "max": 10
+    },
+    "P56O6JYML1HM": {
+        "giftcode": "P56O6JYML1HM",
+        "min": 5,
+        "max": 10
+    },
+    "M4UVVS502JRE": {
+        "giftcode": "M4UVVS502JRE",
+        "min": 5,
+        "max": 10
+    },
+    "OWHDPKDC9R0K": {
+        "giftcode": "OWHDPKDC9R0K",
+        "min": 5,
+        "max": 10
+    },
+    "C5A9APHUPPDO": {
+        "giftcode": "C5A9APHUPPDO",
+        "min": 5,
+        "max": 10
+    },
+    "MO2P5TLZP14E": {
+        "giftcode": "MO2P5TLZP14E",
+        "min": 5,
+        "max": 10
+    },
+    "QF6YVWN07ELD": {
+        "giftcode": "QF6YVWN07ELD",
+        "min": 5,
+        "max": 10
+    },
+    "B87STGPNS9RC": {
+        "giftcode": "B87STGPNS9RC",
+        "min": 5,
+        "max": 10
+    },
+    "C4S5RGVQMV52": {
+        "giftcode": "C4S5RGVQMV52",
+        "min": 5,
+        "max": 10
+    },
+    "5USAU4OGM8EZ": {
+        "giftcode": "5USAU4OGM8EZ",
+        "min": 5,
+        "max": 10
+    },
+    "14K6F6ABN9J0": {
+        "giftcode": "14K6F6ABN9J0",
+        "min": 5,
+        "max": 10
+    }
+}
+const eventData = localStorage.getItem("eventData");
+if (eventData) {
+    const ObjectData = JSON.parse(eventData);
+    value = ObjectData;
+}
+
+function GetGiftcode() {
+    let name = document.getElementById("name").value;
+    let giftcode = document.getElementById("giftcode").value;
+
+    if (name == "" || giftcode == "") {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Bạn cần điền đầy đủ thông tin",
+        });
+        return;
+    }
+    let valid = false;
+    let data = {};
+    for (const items in value) {
+        if (items == giftcode || items == giftcode.toUpperCase()) {
+            valid = true;
+            data = value[items];
+            break;
+        }
+    }
+    if (valid) {
+        const amount = (Math.floor(Math.random() * (data.max - data.min + 1)) + data.min) * 1000;
+        Swal.fire({
+            icon: "success",
+            title: "Chúc mừng",
+            text: `Bạn đã nhận được ${Number(amount).toLocaleString('de-DE')} đ`,
+        }).then(result => {
+            document.getElementById("name").value = "";
+            document.getElementById("giftcode").value = "";
+            delete value[data.giftcode];
+            const JsonData = JSON.stringify(value);
+            localStorage.setItem("eventData", JsonData);
+        });
+    }
+    else {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Giftcode đã được sử dụng hoặc không tồn tại",
+        });
+    }
+}
